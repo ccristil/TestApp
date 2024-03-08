b51b8cdbabbe48956ddfa4befd311c6f813f3284
ccristil <ccristil@byu.edu>
2024-03-07 20:26:23 -0700

    finished the pracitce

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,24 +2,70 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+interface BandProps {
+  name: string;
+  members: string;
+  formed: number;
+}
+
+const bandNames = [
+  {
+    name: 'Greta Van Fleet',
+    members: 'Josh Kiszka, Jake Kiszka, Daniel Wagner, Sam Kiszka, Kyle Hauck',
+    formed: 2012,
+  },
+  {
+    name: 'Journey',
+    members:
+      'Steve Perry, Arnel Pineda, Neal Schon, Randy Jackson, Jonathan Cian, Steve Augeri, Greg Rolie',
+    formed: 1973,
+  },
+  {
+    name: 'Ozzy Osbourne',
+    members: 'Ozzy and friends',
+    formed: 1975,
+  },
+  {
+    name: 'Creed',
+    members:
+      'Scott Stapp, Mark Tremonto, Brian Marshall, Scott Phillips, Brian Brasher, Brett Hestla, Eric Friedman',
+    formed: 1993,
+  },
+];
+
+function Welcome() {
+  return <h1>Some Random Bands that I listen to</h1>;
+}
+
+class Band extends React.Component<BandProps> {
+  render() {
+    const oneBand = this.props;
+
+    return (
+      <div>
+        <img />
+        <h2>Name: {oneBand.name}</h2>
+        <h3>Members: {oneBand.members}</h3>
+        <h3>Formed: {oneBand.formed}</h3>
+      </div>
+    );
+  }
+}
+function BandList() {
+  return (
+    <div>
+      {bandNames.map((bandNum) => (
+        <Band {...bandNum} />
+      ))}
+    </div>
+  );
+}
+
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React. blah blah blah
-        </a>
-        <p>Hello World!!!</p>
-      </header>
+      <Welcome />
+      <BandList />
     </div>
   );
 }
